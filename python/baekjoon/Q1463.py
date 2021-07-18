0eecd46582888cc939417d78771c4bde8c770445
JaeSeong Park <a86007681@gmail.com>
2021-07-18 22:51:55 +0900

    [add solution BOJ(1463)]

---
+++ python/baekjoon/Q1463.py
@@ -0,0 +1,19 @@
+'''
+37012 KB	720 ms
+'''
+
+import sys
+input = sys.stdin.readline
+
+dp = [0] * 1000001
+
+n = int(input())
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+
+print(dp[n])
