d647379b14a100775c180a6730393ae2b1e6f62b
JaeSeong Park <a86007681@gmail.com>
2021-07-24 23:49:35 +0900

    [modify solution BOJ(10844)]

---
+++ python/baekjoon/Q10844.py
@@ -0,0 +1,33 @@
+'''
+29200 KB    72 ms
+'''
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [[0]*10 for _ in range(n+1)]
+
+# print(dp)
+for i in range(1, 10):
+    dp[1][i] = 1
+
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print(sum(dp[n]) % 1000000000)
+
+# for i in range(1, n):
+#     for j in dp[i-1]:
+#         dp[i].append(j)
+#         dp[i].append(j-1)
+#     print(dp[i])
+#     print(sum(dp[i]) % 1000000000)
+# print(sum(dp[n-1]) % 1000000000)
