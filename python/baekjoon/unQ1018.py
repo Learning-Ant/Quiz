3d1e007f1b2254e9438316c38d2792af0edeb23b
JaeSeong Park <a86007681@gmail.com>
2021-08-10 23:40:22 +0900

    [modify solution BOJ(1018)]

--- python/baekjoon/unQ1018.py
+++ python/baekjoon/unQ1018.py
@@ -19,14 +19,25 @@ field = [[1 if i == 'B' else 0 for i in input()] for _ in range(m)]
 '''
 
 count = 0
+answer = 65
 for curM in range(m-8):
     for curN in range(n-8):
         first = field[curM][curN]
-        flag = (curM % 2 == 0)  # True = even, False = odd
         count = 0
         for i in range(8):
+            flag = (i % 2 == 0)  # True = even, False = odd
             for j in range(8):
                 if flag:
                     if j % 2 == 0 and first != field[curM+i][curN+j]:
                         count += 1
-                    elif j
+                    elif j % 2 == 1 and first == field[curM+i][curN+j]:
+                        count += 1
+                else :
+                    if j % 2 == 0 and first == field[curM+i][curN+j]:
+                        count += 1
+                    elif j % 2 == 1 and first != field[curM+i][curN+j]:
+                        count += 1
+        
+        answer = min(answer, count)
+
+print(answer)
