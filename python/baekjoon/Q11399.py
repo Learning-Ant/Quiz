7e5d5d99919a7f1219ab278523316600ca5589c8
JaeSeong Park <a86007681@gmail.com>
2021-08-23 20:51:01 +0900

    [add solution BOJ(11399)]

---
+++ python/baekjoon/Q11399.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+times = sorted(list(map(int, input().split())))
+
+for i in range(1, n):
+  times[i] += times[i - 1]
+
+print(sum(times))
