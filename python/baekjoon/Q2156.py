d04a4bed6ced1fe51f27b585ad4becb1f40ce475
JaeSeong Park <a86007681@gmail.com>
2021-08-31 22:05:00 +0900

    [add solution BOJ(2156)]

--- python/baekjoon/Q2156.py
+++ python/baekjoon/Q2156.py
@@ -1,3 +1,15 @@
+'''
+세 가지 경우를 생각한다.
+
+1. 현재 잔을 마시지 않는 경우
+  1-1. 이전 잔을 마시는 경우
+  1-2. 전전잔 + 이전잔을 마시는 경우
+2. 현재 잔을 마시는 경우
+  2-1. 이전 잔을 마시는 경우
+
+29708 KB	84 ms
+'''
+
 import sys
 
 input = sys.stdin.readline
@@ -12,5 +24,4 @@ dp[1] = wines[0]
 for i in range(2, n + 1):
   dp[i] = max(dp[i - 2] + wines[i - 1], dp[i - 3] + wines[i - 2] + wines[i - 1], dp[i - 1])
 
-print(dp)
 print(dp[n])
