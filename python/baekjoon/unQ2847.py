6d88f29f118c59acf97cc2309a796213d3e5b250
JaeSeong Park <a86007681@gmail.com>
2021-09-09 21:39:01 +0900

    [add unsolved solution BOJ(2847)]

---
+++ python/baekjoon/unQ2847.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+levelScores = [int(input().strip()) for _ in range(n)]
+cnt = 0
+
+for i in range(n-1, 1, -1):
+  if levelScores[i-1] >= levelScores[i]:
+    cnt = levelScores[i-1] - levelScores[i] + 1
+    levelScores[i-1] = levelScores[i] - 1
+
+print(levelScores)
+print(cnt)
