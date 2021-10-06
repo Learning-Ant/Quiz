6a5a160747d0a6431086eb6e436b2778b15f302f
JaeSeong Park <a86007681@gmail.com>
2021-10-07 00:00:27 +0900

    [modify solution BOJ(1764)]

--- python/baekjoon/Q1764.py
+++ python/baekjoon/Q1764.py
@@ -1,15 +1,29 @@
+'''
+자료형 set와 연산자 in을 활용한 판별
+
+좀 더 쉽게 set의 intersection을 활용해 푸는 방법도 가능하다.
+set끼리의 intersection은 set의 함수를 사용할 수도 있고,
+& 연산자를 통해서도 구할 수 있다.
+
+ex)
+set([1, 2, 3]).intersection([2, 3, 4])
+{[1, 2, 3]} & {[2, 3, 4]}
+
+40136 KB	124 ms
+'''
+
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
 h = [input().strip() for _ in range(n)]
-s = [input().strip() for _ in range(m)]
+s = set([input().strip() for _ in range(m)])
 
 sh = []
 
 for i in h:
-  if s.count(i) == 1:
+  if i in s:
     sh.append(i)
 
 print(len(sh))
