64147629db9f36d4f73514a3ac6eb8d4bc70be38
JaeSeong Park <a86007681@gmail.com>
2021-10-07 23:54:54 +0900

    [add unsolved BOJ(7795)]

---
+++ python/baekjoon/Q7795.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def biSearch(b:list, )
+
+tc = int(input())
+
+for _ in range(tc) :
+  n, m = map(int, input().strip().split())
+  nl = sorted(list(map(int, input().strip().split())))
+  ml = sorted(list(map(int, input().strip().split())))
+
+  
+
